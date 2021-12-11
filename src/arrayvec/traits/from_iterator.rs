d6dfb10036c44a2a2e918da731f480ecd846bbe3
Rafael Buchbinder <rafi@rbk.io>
2021-12-11 08:19:18 +0200

    arrayvec: add test coverage for FromIterator

--- src/arrayvec/traits/from_iterator.rs
+++ src/arrayvec/traits/from_iterator.rs
@@ -24,3 +24,40 @@ where
         Self::try_from_iter(i).expect("insufficient capacity")
     }
 }
+
+#[cfg(test)]
+mod testing {
+    use crate as cds;
+    use cds::{
+        arrayvec::ArrayVec,
+        defs::{Uninitialized, Usize},
+        testing::dropped::{Dropped, Track},
+    };
+
+    #[test]
+    fn test_from_iter() {
+        type A<'a> = ArrayVec<Dropped<'a, 16>, Usize, Uninitialized, 16>;
+        let t = Track::<16>::new();
+        let a = A::from_iter(t.take(5));
+        assert_eq!(t.n_allocated(), 5);
+        assert!(t.dropped_range(0..0)); // empty range
+
+        drop(a);
+        assert_eq!(t.n_allocated(), 0);
+        assert!(t.dropped_range(0..=4));
+    }
+
+    #[test]
+    fn test_from_iter_copy() {
+        type A = ArrayVec<usize, Usize, Uninitialized, 8>;
+        let a = A::from_iter(0..5);
+        assert_eq!(a, [0, 1, 2, 3, 4]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_from_iter_panics_on_capacity_error() {
+        type A = ArrayVec<usize, Usize, Uninitialized, 8>;
+        let _a = A::from_iter(0..9);
+    }
+}
