fb5fdfccc8db62f6bb4f4a486eb0b8ff8f7a2c00
Rafael Buchbinder <rafi@rbk.io>
2021-12-11 17:13:29 +0200

    arrayvec: add test coverage for Ord

--- src/arrayvec/traits/ord.rs
+++ src/arrayvec/traits/ord.rs
@@ -27,3 +27,36 @@ where
         Ord::cmp(&**self, &**other)
     }
 }
+
+#[cfg(test)]
+mod testing {
+    use crate as cds;
+    use cds::array_vec;
+    use core::cmp::Ordering;
+
+    #[test]
+    fn test_partial_ord() {
+        let a = array_vec![3; u64; 1, 2, 3];
+        let b = array_vec![3; u64; 2, 2, 3];
+
+        assert!(a < b);
+        assert!(a <= b);
+        assert!(!(a >= b));
+        assert!(!(a > b));
+
+        assert!(b > a);
+        assert!(b >= a);
+        assert!(!(b <= a));
+        assert!(!(b < a));
+    }
+
+    #[test]
+    fn test_ord() {
+        let a = array_vec![3; u64; 1, 2, 3];
+        let b = array_vec![3; u64; 2, 2, 3];
+
+        assert_eq!(a.cmp(&b), Ordering::Less);
+        assert_eq!(b.cmp(&a), Ordering::Greater);
+        assert_eq!(a.cmp(&a), Ordering::Equal);
+    }
+}
