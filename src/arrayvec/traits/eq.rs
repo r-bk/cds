438332c4ed8a8e806507779308a11517958415c5
Rafael Buchbinder <rafi@rbk.io>
2021-12-11 16:41:33 +0200

    arrayvec: add test coverage for PartialEq

--- src/arrayvec/traits/eq.rs
+++ src/arrayvec/traits/eq.rs
@@ -83,3 +83,61 @@ where
 }
 
 impl<T: Eq, L: LengthType, SM: SpareMemoryPolicy<T>, const C: usize> Eq for ArrayVec<T, L, SM, C> {}
+
+#[cfg(test)]
+mod testing {
+    use crate as cds;
+    use cds::{
+        array_vec,
+        arrayvec::ArrayVec,
+        defs::{Uninitialized, Usize},
+    };
+
+    type A = ArrayVec<u64, Usize, Uninitialized, 7>;
+
+    #[test]
+    fn test_eq_arr_ref() {
+        let a = A::from_iter(0..3);
+        assert!(a == &[0, 1, 2]);
+        assert!(a != &[0, 1, 2, 3]);
+    }
+
+    #[test]
+    fn test_eq_arr() {
+        let a = A::from_iter(1..3);
+        assert!(a == [1, 2]);
+        assert!(a != [1]);
+    }
+
+    #[test]
+    fn test_eq_slice_ref() {
+        let a = A::from_iter((100..102).rev());
+        assert!(a == [101u64, 100].as_ref());
+        assert!(a != [100].as_ref());
+    }
+
+    #[test]
+    fn test_eq_slice() {
+        let a = A::from_iter(3..5);
+        let arr1: [u64; 2] = [3, 4];
+        let arr2: [u64; 2] = [5, 6];
+        assert!(a == arr1[..]);
+        assert!(a != arr2[..]);
+    }
+
+    #[test]
+    fn test_eq_av_ref() {
+        let a = A::from_iter(0..2);
+        let b = array_vec![2; u64; 0, 1];
+        assert!(a == &b);
+        assert!(a != &array_vec![7; u64]);
+    }
+
+    #[test]
+    fn test_eq_av() {
+        let a = A::from_iter(0..2);
+        let b = array_vec![2; u64; 0, 1];
+        assert!(a == b);
+        assert!(a != array_vec![7; u64]);
+    }
+}
