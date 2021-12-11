292dc14a82d789fecf5c24ceb96a7900161ba329
Rafael Buchbinder <rafi@rbk.io>
2021-12-11 16:09:06 +0200

    arrayvec: add test coverage for CapacityError

--- src/errors.rs
+++ src/errors.rs
@@ -15,3 +15,36 @@ impl core::fmt::Display for CapacityError {
 #[cfg(feature = "std")]
 #[cfg_attr(docsrs, doc(cfg(feature = "std")))]
 impl std::error::Error for CapacityError {}
+
+#[cfg(all(test, feature = "std"))]
+mod testing {
+    use super::*;
+
+    #[test]
+    fn test_capacity_error_display() {
+        let e = CapacityError {};
+        let s = format!("{}", e);
+        assert_eq!(s, "insufficient capacity");
+    }
+
+    #[test]
+    fn test_capacity_error_debug() {
+        let e = CapacityError {};
+        let s = format!("{:?}", e);
+        assert_eq!(s, "CapacityError");
+    }
+
+    #[test]
+    fn test_capacity_error_copy() {
+        let e = CapacityError {};
+        let e2 = e;
+        format!("{} {}", e, e2);
+    }
+
+    #[test]
+    fn test_capacity_error_clone() {
+        let e = CapacityError {};
+        let e2 = e.clone();
+        format!("{} {}", e, e2);
+    }
+}
