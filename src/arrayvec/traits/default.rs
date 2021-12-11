574bd1dc862db78fb360d7ca72b2ef6b476d7d8e
Rafael Buchbinder <rafi@rbk.io>
2021-12-11 07:33:09 +0200

    arrayvec: add test coverage for Default

--- src/arrayvec/traits/default.rs
+++ src/arrayvec/traits/default.rs
@@ -14,3 +14,21 @@ where
         Self::new()
     }
 }
+
+#[cfg(test)]
+mod testing {
+    use crate as cds;
+    use cds::{
+        arrayvec::ArrayVec,
+        defs::{Uninitialized, U8},
+    };
+
+    #[test]
+    fn test_default() {
+        type A = ArrayVec<u8, U8, Uninitialized, 7>;
+        let a: A = Default::default();
+        assert_eq!(a.len(), 0);
+        assert_eq!(a.capacity(), 7);
+        assert_eq!(a.spare_capacity_len(), a.capacity());
+    }
+}
