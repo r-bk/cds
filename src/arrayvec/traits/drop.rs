2e2db600cfb39c7c54a1b67bf1f19bd325ead96b
Rafael Buchbinder <rafi@rbk.io>
2021-12-11 07:40:43 +0200

    arrayvec: add test coverage for Drop

--- src/arrayvec/traits/drop.rs
+++ src/arrayvec/traits/drop.rs
@@ -14,3 +14,33 @@ where
         self.truncate(0)
     }
 }
+
+#[cfg(test)]
+mod testing {
+    use crate as cds;
+    use cds::{
+        arrayvec::ArrayVec,
+        defs::{Uninitialized, Usize},
+        testing::dropped::{Dropped, Track},
+    };
+
+    #[test]
+    fn test_drop() {
+        type A<'a> = ArrayVec<Dropped<'a, 16>, Usize, Uninitialized, 8>;
+        let t = Track::<16>::new();
+        let a = A::try_from_iter(t.take(5)).unwrap();
+        assert_eq!(t.n_allocated(), 5);
+        assert!(t.dropped_range(0..0)); // empty range
+
+        drop(a);
+        assert_eq!(t.n_allocated(), 0);
+        assert!(t.dropped_range(0..=4));
+    }
+
+    #[test]
+    fn test_drop_copy() {
+        type A = ArrayVec<usize, Usize, Uninitialized, 7>;
+        let a = A::try_from_iter((0..5).into_iter()).unwrap();
+        drop(a);
+    }
+}
