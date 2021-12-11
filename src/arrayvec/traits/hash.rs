4bd12c44bb6c26bb4f72456c3b6ac1304962da76
Rafael Buchbinder <rafi@rbk.io>
2021-12-11 09:58:59 +0200

    arrayvec: add test coverage for Hash

--- src/arrayvec/traits/hash.rs
+++ src/arrayvec/traits/hash.rs
@@ -15,3 +15,24 @@ where
         Hash::hash(&**self, state)
     }
 }
+
+#[cfg(all(test, feature = "std"))]
+mod testing {
+    use crate as cds;
+    use cds::array_vec;
+    use core::hash::{Hash, Hasher};
+    use std::collections::hash_map::DefaultHasher;
+
+    #[test]
+    fn test_hash() {
+        let mut hasher1 = DefaultHasher::new();
+        let a = array_vec![3; u64; 3, 1, 2];
+        a.hash(&mut hasher1);
+
+        let mut hasher2 = DefaultHasher::new();
+        let arr: [u64; 3] = [3, 1, 2];
+        arr.hash(&mut hasher2);
+
+        assert_eq!(hasher1.finish(), hasher2.finish());
+    }
+}
