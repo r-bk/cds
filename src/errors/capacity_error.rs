d1c48dcb904ae21ffad6d85986c1da349bc6031a
Rafael Buchbinder <rafi@rbk.io>
2021-12-18 11:22:02 +0200

    errors: add CapacityErrorVal and InsertErrorVal types
    To be used in try_push_val and try_insert_val methods.

--- src/errors/capacity_error.rs
+++ src/errors/capacity_error.rs
@@ -1,4 +1,11 @@
-use core::fmt::Formatter;
+use core::{
+    any::type_name,
+    clone::Clone,
+    fmt::{Debug, Display, Formatter},
+    marker::Copy,
+};
+
+// ----------------------------------------------------------------------------
 
 /// An error returned when there is no free capacity in a collection.
 #[derive(Debug, Copy, Clone)]
@@ -13,6 +20,30 @@ impl core::fmt::Display for CapacityError {
 #[cfg(feature = "std")]
 #[cfg_attr(docsrs, doc(cfg(feature = "std")))]
 impl std::error::Error for CapacityError {}
+
+// ----------------------------------------------------------------------------
+
+/// An error returned with a value when there is no free capacity in a collection.
+#[derive(Copy, Clone)]
+pub struct CapacityErrorVal<T>(pub T);
+
+impl<T> Display for CapacityErrorVal<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        write!(f, "insufficient capacity")
+    }
+}
+
+impl<T> Debug for CapacityErrorVal<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        write!(f, "CapacityErrorVal<{}>", type_name::<T>())
+    }
+}
+
+#[cfg(feature = "std")]
+#[cfg_attr(docsrs, doc(cfg(feature = "std")))]
+impl<T> std::error::Error for CapacityErrorVal<T> {}
+
+// ----------------------------------------------------------------------------
 
 #[cfg(all(test, feature = "std"))]
 mod testing {
@@ -44,5 +75,27 @@ mod testing {
         let e = CapacityError {};
         let e2 = e.clone();
         format!("{} {}", e, e2);
+    }
+
+    #[test]
+    fn test_capacity_error_val_display() {
+        let e = CapacityErrorVal::<u64>(17);
+        let s = format!("{}", e);
+        assert_eq!(s, "insufficient capacity")
+    }
+
+    #[test]
+    fn test_capacity_error_val_debug() {
+        let e = CapacityErrorVal::<u64>(717);
+        let s = format!("{:?}", e);
+        assert_eq!(s, "CapacityErrorVal<u64>")
+    }
+
+    #[test]
+    fn test_capacity_error_val_clone() {
+        let e = CapacityErrorVal::<String>("-11".into());
+        let c = e.clone();
+        assert_eq!(e.0, c.0);
+        assert_eq!(e.0, "-11");
     }
 }
--- src/errors/insert_error.rs
+++ src/errors/insert_error.rs
@@ -1,4 +1,11 @@
-use core::fmt::Formatter;
+use core::{
+    any::type_name,
+    clone::Clone,
+    fmt::{Debug, Display, Formatter},
+    marker::Copy,
+};
+
+// ----------------------------------------------------------------------------
 
 /// An error returned from `try_insert` method.
 #[derive(Debug, Copy, Clone)]
@@ -23,6 +30,63 @@ impl core::fmt::Display for InsertError {
 #[cfg_attr(docsrs, doc(cfg(feature = "std")))]
 impl std::error::Error for InsertError {}
 
+// ----------------------------------------------------------------------------
+
+/// An error returned from `try_insert_val` method.
+#[derive(Copy, Clone)]
+pub enum InsertErrorVal<T> {
+    /// Requested index is out of collection bounds.
+    IndexOutOfBounds(T),
+
+    /// There is no spare capacity to accommodate a new element.
+    CapacityError(T),
+}
+
+impl<T> InsertErrorVal<T> {
+    /// Returns a reference to the value conveyed by the error.
+    #[inline]
+    pub fn value(&self) -> &T {
+        match self {
+            Self::IndexOutOfBounds(v) => v,
+            Self::CapacityError(v) => v,
+        }
+    }
+
+    /// Returns the value conveyed by the error.
+    #[inline]
+    pub fn into_value(self) -> T {
+        match self {
+            Self::IndexOutOfBounds(v) => v,
+            Self::CapacityError(v) => v,
+        }
+    }
+}
+
+impl<T> Display for InsertErrorVal<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        match self {
+            Self::IndexOutOfBounds(_) => write!(f, "index is out of bounds"),
+            Self::CapacityError(_) => write!(f, "insufficient capacity"),
+        }
+    }
+}
+
+impl<T> Debug for InsertErrorVal<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        let name = type_name::<T>();
+        match self {
+            Self::IndexOutOfBounds(_) => write!(f, "InsertErrorVal<{}>::IndexOutOfBounds", name),
+            Self::CapacityError(_) => write!(f, "InsertErrorVal<{}>::CapacityError", name),
+        }
+    }
+}
+
+#[cfg(feature = "std")]
+#[cfg_attr(docsrs, doc(cfg(feature = "std")))]
+impl<T> std::error::Error for InsertErrorVal<T> {}
+
+// ----------------------------------------------------------------------------
+
 #[cfg(all(test, feature = "std"))]
 mod testing {
     use super::*;
@@ -43,5 +107,62 @@ mod testing {
         let e = InsertError::IndexOutOfBounds;
         let s = format!("{:?}", e);
         assert_eq!(s, "IndexOutOfBounds");
+    }
+
+    #[test]
+    fn test_insert_error_val_display() {
+        let e = InsertErrorVal::<u64>::IndexOutOfBounds(7);
+        let s = format!("{}", e);
+        assert_eq!(s, "index is out of bounds");
+
+        let e = InsertErrorVal::<u64>::CapacityError(17);
+        let s = format!("{}", e);
+        assert_eq!(s, "insufficient capacity");
+    }
+
+    #[test]
+    fn test_insert_error_val_debug() {
+        let e = InsertErrorVal::<u64>::IndexOutOfBounds(7);
+        let s = format!("{:?}", e);
+        assert_eq!(s, "InsertErrorVal<u64>::IndexOutOfBounds");
+
+        let e = InsertErrorVal::<u64>::CapacityError(17);
+        let s = format!("{:?}", e);
+        assert_eq!(s, "InsertErrorVal<u64>::CapacityError");
+    }
+
+    #[test]
+    fn test_insert_error_val_copy() {
+        let e = InsertErrorVal::<u64>::IndexOutOfBounds(17);
+        let e2 = e;
+        assert_eq!(e.value(), e2.value());
+
+        let e = InsertErrorVal::<u64>::CapacityError(717);
+        let e2 = e;
+        assert_eq!(e.value(), e2.value());
+    }
+
+    #[test]
+    fn test_insert_error_val_clone() {
+        let e = InsertErrorVal::<String>::IndexOutOfBounds("17".into());
+        let e2 = e.clone();
+        assert_eq!(e.value(), e2.value());
+        assert_eq!(e.value(), "17");
+
+        let e = InsertErrorVal::<String>::CapacityError("717".into());
+        let e2 = e.clone();
+        assert_eq!(e.value(), e2.value());
+        assert_eq!(e.value(), "717");
+    }
+
+    #[test]
+    fn test_insert_error_val_into_value() {
+        let e = InsertErrorVal::<String>::IndexOutOfBounds("Hello, world!".into());
+        let v = e.into_value();
+        assert_eq!(v, "Hello, world!");
+
+        let e = InsertErrorVal::<String>::CapacityError("Hello again!".into());
+        let v = e.into_value();
+        assert_eq!(v, "Hello again!");
     }
 }
